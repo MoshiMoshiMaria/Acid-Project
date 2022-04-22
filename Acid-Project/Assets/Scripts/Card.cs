16effbeb03a4bb2d0aa7f4f0438651f6b10a868e
Daniel Clipston <daniel_clipston@hotmail.com>
2022-04-22 16:44:56 +0100

    Added start to card script, added beginning card template
    Card Template is a basic layout prefab, using basic materials and UI elements to show where each element will end up once rudimentary art has been made

---
+++ Acid-Project/Assets/Scripts/Card.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Card
+{
+    public string s_name;//!< Name of the Card
+    public int i_action;//!< Action of the card - int code will define action, extra parameters supplied elsewhere
+    public List<int> l_extraInfo;//!< Extra info needed by the action in order to work - length will depend on the action
+    public int i_cost;//!< Cost of the card to perform action
+    public int i_moveLength;//!< How much movement the card can be burned for
+
+    public string s_description;//!< Description of the card and it's use
+
+    [SerializeField]
+    private Image image;//!< Image to be displayed on the card
+
+    /*! \function Card
+     * \brief Constructor for the Card class
+     * Constructor takes the name, action code and the extra information
+     */
+    public Card(string name, int action, List<int> extraInfo)
+    {
+        s_name = name;
+        i_action = action;
+        //l_extraInfo = new List<int>(extraInfo.Count); //Doing this an easier way now
+        l_extraInfo = extraInfo;
+
+        //load image with same name
+    }
+
+    /*! \function DisplayCard
+     * \brief Function to display a card on a given template
+     * Each CardTemplate object will be modified by this class inorder to 
+     */
+    //public void DisplayCard(CardTemplate displayCanvas)
+    //{
+    //    //Unimplemented
+    //}
+}
